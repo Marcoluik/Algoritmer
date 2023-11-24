e0d39eec4077be243a1782051565fed1e1468657
Marco <marcokot@icloud.com>
2023-11-24 15:26:15 +0100

    ggs

---
+++ Recursion.py
@@ -0,0 +1,37 @@
+#Recursion
+
+# 1 call itself,
+# 2 Base case, stop tidspunkt
+# 3 Change variable
+
+
+import turtle
+
+
+def tree(branchLen, t):
+    if branchLen > 10:
+        t.forward(branchLen)
+        t.right(40)
+        tree(branchLen - 30, t)
+        t.left(80)
+        tree(branchLen - 30, t)
+        t.right(40)
+        t.backward(branchLen)
+
+
+def main():
+    myWin = turtle.Screen()
+    myWin.bgcolor("black")
+    t = turtle.Turtle()
+    t.width(10)
+    t.left(90)
+    t.up()
+
+    t.backward(100)
+    t.down()
+    t.color("silver")
+    tree(150, t)
+    myWin.exitonclick()
+
+
+main()
